821cdac6750b4514661fe41de0ae5bb00f6edeae
akarimkesheva <akarimkesheva@ivytech.edu>
2026-02-01 16:54:25 -0500

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+const searchInput = document.getElementById('search');
+const jobsList = document.querySelectorAll('.right-column article');
+
+
+searchInput.addEventListener('keyup', (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+
+    jobsList.forEach(item => {
+        const title = item.querySelector('h3').textContent.toLowerCase();
+        const description = item.querySelector('p').textContent.toLowerCase();
+
+
+        if (title.includes(searchTerm) || description.includes(searchTerm)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
